aa696a8daa9fb6e9e79b18139cd1a58ee78a7877
TheApple <103326417+TheApple1@users.noreply.github.com>
2023-08-20 11:40:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Designer", "Game Developer", "Student" ,"Cyclist"],
+        strings: ["Web Designer", "Game Developer", "Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
